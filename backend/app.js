df639b10c76742cce6109c0aacaddc476859de3e
Sarah <sarahfong@outlook.fr>
2019-12-10 13:16:29 +0100

    find, findAll, delete, add for BDD coach

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,30 @@
+var createError = require('http-errors');
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/BDD_dashboard', {useNewUrlParser: true}, (err) => {
+    if (!err) {
+        console.log('Successfully Established Connection with MongoDB')
+    }
+    else {
+        console.log('Failed to Establish Connection with MongoDB with Error: '+ err)
+    }
+});
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var summonerRouter = require('./routes/summoners');
 var matchRouter = require('./routes/matches');/*
 var championRouter = require('./routes/champions');*/
+var coachRouter = require('./routes/coach');
+var coachesRouter = require('./routes/coaches');
 
 var app = express();
 
@@ -20,5 +37,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use('/summoners', summonerRouter);
 app.use('/matches', matchRouter);
+app.use('/coach', coachRouter);
+app.use('/coaches', coachesRouter);
 
 module.exports = app;
---
+++ backend/controllers/coach.controller.js
@@ -0,0 +1,96 @@
+const Coach = require('../models/coach.model.js');
+
+// Return all the coaches from the db
+exports.findAll = (req, res) => {
+    Coach.find()
+        .then(coaches => {
+            console.log(coaches);
+            res.send(coaches);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while retrieving users.'
+            });
+        });
+};
+
+
+// Return one coach by its name from the db
+exports.findOne = (req, res) => {
+
+    Coach.findOne({'name': req.body.name})
+        .then(coaches => {
+            console.log(coaches);
+            res.send(coaches);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while retrieving users.'
+            });
+        });
+
+};
+
+
+// Create a new coach in the db
+exports.create = (req, res) => {
+    /*console.log(req.body);*/
+    // Request all the informations required in collection coach
+    if (!req.body.name || !req.body.nbTeams) {
+        return res.status(400).send({
+            message: 'name and nbTeams can not be empty'
+        });
+    }
+
+    // Create a new coach
+    const coach = new Coach({
+        name: req.body.name,
+        nbTeams: req.body.nbTeams
+    });
+    console.log(coach);
+
+    // Save the new coach in the db and returns it
+    coach
+        .save()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            // Error message
+            res.status(500).send({
+                message: err.message || 'Cannot create a new coach.'
+            });
+        });
+};
+
+
+/*// Delete a User with the specified UserId in the request
+exports.delete = (req, res) => {
+    Coach.findByIdAndDelete(req.body.coachId)
+        .then(coach => {
+            if (!coach) {
+                return res.status(404).send({
+                    message: 'Coach ${req.body.name} not found'
+                });
+            }
+            res.send({ message: `Coach ${req.body.name} deleted` });
+        })
+
+};*/
+
+// delete a coach by its name
+exports.delete = (req, res) => {
+    // find the coach
+    Coach.findOneAndDelete({'name': req.body.name})
+        .then(coach => {
+            // if not found : error message
+            if (!coach) {
+                return res.status(404).send({
+                    message: 'Coach not found'
+                });
+            }
+            //if found : return a message
+            res.send({ message: `Coach ${req.body.name} deleted` });
+        });
+};
+
